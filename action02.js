855636f7f5b19ba529874e29b39a925e552cf9a7
Henry Yoon <pkower4@gmail.com>
2021-07-27 15:31:44 +0900

    Update action02.js

--- action02.js
+++ action02.js
@@ -10,8 +10,8 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'EntTangunGHEC',
-    repo: 'KakaoSeminar',
+    owner: 'wooody92',
+    repo: 'antions',
     issue_number: core.getInput('issue_number'),
     labels: [
       'bug'
